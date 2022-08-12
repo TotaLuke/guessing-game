62144ec8bf7bf383618b5567f8eab6fbcb98de9d
TotaLukem <nickola.anelka@gmail.com>
2022-08-12 15:18:55 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.average = Math.round((this.min + this.max)/2);
+        return this.average;
     }
 
     lower() {
-
+        this.max = this.average;
     }
 
     greater() {
-
+        this.min = this.average;
     }
 }
 
